8b920c7a38050571f5d30a23e38db37ebb6b0118
Unknown <thomaspoint@gmail.com>
2017-11-06 15:47:36 +0100

    Basic

--- View/header.php
+++ View/header.php
@@ -1,5 +1,13 @@
 <div class="header">
-  <a href="Anglais">English</a>
-  <a href="Francais">Francais</a>
+  <div class="head">
+    <ul>
+      <li>Antiquité</li>
+      <li>Décoration</li>
+      <li>Agencement</li>
+    </ul>
+  </div>
+  <div class="langues">
+    <a href="Anglais">English</a>
+    <a href="Francais">Francais</a>
+  </div>
 </div>
-  
