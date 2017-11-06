c17a19296935f42e044e175c4a4a2dee92828fb2
Unknown <thomaspoint@gmail.com>
2017-11-06 15:19:14 +0100

    puuush

--- View/footer.php
+++ View/footer.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-  <body>
-    <div class="footer">
-      <p>© 2017 Didier Motte +32 495 20 59 34 - <a href "" alt="Email">dmotte@skynet.be</a> - <a href "/contact.php" alt="Plan d'accès">Plan d'accès</a></p>
-      <p>Décorateur, Antiquaire, agencement d'intérieurs, Belgique</p>
-      <p>Stay update via: <a href="https://www.facebook.com/antiquites.decoration" alt="Facebook" target=_blank>Facebook</a></p>
-      <p>Designed & Developed by <a href="https://pixeline.be/" alt="Pixeline" target=_blank>pixeline</a></p>
-    </div>
-  </body>
-</html>
+<div class="footer">
+  <p>© 2017 Didier Motte +32 495 20 59 34 - <a href "" alt="Email">dmotte@skynet.be</a> - <a href="/contact.php" alt="Plan d'accès">Plan d'accès</a></p>
+  <p>Décorateur, Antiquaire, agencement d'intérieurs, Belgique</p>
+  <p>Stay update via: <a href="https://www.facebook.com/antiquites.decoration" alt="Facebook" target=_blank>Facebook</a></p>
+  <p>Designed & Developed by <a href="https://pixeline.be/" alt="Pixeline" target=_blank>pixeline</a></p>
+</div>
--- View/header.php
+++ View/header.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-  <body>
-    <div class="header">
-      <a href="Anglais">English</a>
-      <a href="Francais">Francais</a>
-    </div>
-  </body>
-</html>
+<div class="header">
+  <a href="Anglais">English</a>
+  <a href="Francais">Francais</a>
+</div>
+  
