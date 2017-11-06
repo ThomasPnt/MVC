b5a066b249ede9b65739e9640e59e49381496945
Unknown <thomaspoint@gmail.com>
2017-11-06 14:46:40 +0100

    Footer-header

---
+++ View/footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+  <body>
+    <div class="footer">
+      <p>© 2017 Didier Motte +32 495 20 59 34 - <a href="Email">dmotte@skynet.be</a> - <a href="Plan d'accès">Plan d'accès</a></p>
+      <p>Décorateur, Antiquaire, agencement d'intérieurs, Belgique</p>
+      <p>Stay update via: <a href="Facebook">Facebook</a></p>
+      <p>Designed & Developed by <a href="Pixeline">pixeline</a></p>
+    </div>
+  </body>
+</html>
