8d8a93b16e9e3b696172ac28218c6585f5c5f39e
debraive <32301367+debraive@users.noreply.github.com>
2017-11-07 16:30:30 +0100

    css

--- View/presentation.php
+++ View/presentation.php
@@ -1,16 +1,17 @@
+<link rel="stylesheet" href="css/styles.css">
 <div id="container">
   <div>
     <h1>DIDIER MOTTE</h1>
-    <img src="https://didiermotte.be/wp-content/uploads/2013/08/didier-grosplan-bw-o.png" 
+    <img src="https://didiermotte.be/wp-content/uploads/2013/08/didier-grosplan-bw-o.png"
     alt="Picture of Didier Motte"/>
     <h3>Les meubles ont une histoire, authentique.</h3>
-    <p>Toujours à la recherche de l’extra-ordinaire, de l’insolite, fasciné par la « petite » histoire 
-    du monde, celle des métiers d’autrefois, Didier Motte fait partie de ces quelques connaisseurs, 
-    brocanteurs ou décorateurs qui, à travers leur regard et leur toucher, savent percevoir l’histoire 
+    <p>Toujours à la recherche de l’extra-ordinaire, de l’insolite, fasciné par la « petite » histoire
+    du monde, celle des métiers d’autrefois, Didier Motte fait partie de ces quelques connaisseurs,
+    brocanteurs ou décorateurs qui, à travers leur regard et leur toucher, savent percevoir l’histoire
     des objets anciens pour leur donner une nouvelle légitimité esthétique et fonctionnelle.</p>
-    <q>Je fais essentiellement des anciens meubles de commerce, des intérieurs complets de vieux magasins, 
-    bistrots, pharmacies… J’ai un entrepôt à Luttre, à Nivelles et à Haerelbeek . Si vous êtes dans le 
-    coin, vous pouvez venir jeter un coup d’oeil ( appelez-moi auparavant au 0495 205 934 ).  
+    <q>Je fais essentiellement des anciens meubles de commerce, des intérieurs complets de vieux magasins,
+    bistrots, pharmacies… J’ai un entrepôt à Luttre, à Nivelles et à Haerelbeek . Si vous êtes dans le
+    coin, vous pouvez venir jeter un coup d’oeil ( appelez-moi auparavant au 0495 205 934 ).
     À bientôt! – Didier.</q>
     <h3>Services</h3>
     <p>Location de meubles et accessoires pour tournage.</p>
@@ -18,13 +19,13 @@
     <p>Livraison dans toute l’Europe et le monde.</p>
     <h3>Ils ont fait confiance</h3>
     <p><a href="http://www.zahia.be/">ZAHIA</a> : ( Anvers , Gand , Brugge , Hasselt  ) Bijoux .</p>
-    <p><a href="http://www.vino-events.com/">VINOPRES</a> : ( Bruxelles ) Publications sur le monde 
+    <p><a href="http://www.vino-events.com/">VINOPRES</a> : ( Bruxelles ) Publications sur le monde
     du vin .</p>
-    <p><a href="http://le-relais-du-triporteur.be/#home">LE RELAIS DU TRIPOREUR</a> : ( Bruxelles ) 
+    <p><a href="http://le-relais-du-triporteur.be/#home">LE RELAIS DU TRIPOREUR</a> : ( Bruxelles )
     Epicerie et restaurant Bio .</p>
-    <p><a href="http://www.superfillesdutram.com/">LES SUPER FILLES DU TRAM</a> : ( Bruxelles ) 
+    <p><a href="http://www.superfillesdutram.com/">LES SUPER FILLES DU TRAM</a> : ( Bruxelles )
     Restaurant .</p>
-    <p><a href="http://www.intaglio.fr/">INTAGLIO</a> : ( Paris ) Imprimeur , graveur , papier 
+    <p><a href="http://www.intaglio.fr/">INTAGLIO</a> : ( Paris ) Imprimeur , graveur , papier
     de créations .</p>
     <p><a href="http://www.villa-alice.be/">VILLA ALICE</a> : ( Westande-bad ) Restaurant .</p>
     <p>MAMMUTI : ( Ile de Ré ) Galerie , appartements d’hôtes .</p>
