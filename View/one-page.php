e2643b60166893b7ab029da3b3fe0ddf1ade7f26
Unknown <thomaspoint@gmail.com>
2017-11-09 16:37:25 +0100

    Onepage

---
+++ View/one-page.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Didier Motte</title>
+  <link rel="stylesheet" href="css/one-page.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+</head>
+  <body>
+    <div id="container">
+      <div id="presentation">
+        <h1>Didier Motte</h1>
+        <p id="firstpara">
+          Toujours à la recherche de l’extra-ordinaire, de l’insolite, fasciné par la « petite » histoire
+          du monde, celle des métiers d’autrefois, Didier Motte fait partie de ces quelques connaisseurs,
+          brocanteurs ou décorateurs qui, à travers leur regard et leur toucher, savent percevoir l’histoire
+          des objets anciens pour leur donner une nouvelle légitimité esthétique et fonctionnelle.
+        </p>
+        <p id='italique'>
+          Je fais essentiellement des anciens meubles de commerce, des intérieurs complets de vieux magasins,
+          bistrots, pharmacies… J’ai un entrepôt à Luttre, à Nivelles et à Haerelbeek . Si vous êtes dans le
+          coin, vous pouvez venir jeter un coup d’oeil ( appelez-moi auparavant au 0495 205 934 ).
+          À bientôt! – Didier.
+        </p>
+        <ul>
+          <li>Services</li>
+          <li>Livraison</li>
+        </ul>
+      </div>
+      <div id="stock">
+        <h1>Stock</h1>
+        <div class="image">
+          <div class="imgs"><img class = "stock" src="https://cdn.laredoute.com/products/250by250/0/6/c/06c48931d22a7c4a5e98ebb79aa8ef1d.jpg" alt="image meuble"><p>Vitrine</p></div>
+          <div class="imgs"><img class = "stock" src="https://cdn.laredoute.com/products/250by250/0/6/c/06c48931d22a7c4a5e98ebb79aa8ef1d.jpg" alt="image meuble"><p>Grand mirroir</p></div>
+          <div class="imgs"><img class = "stock" src="https://cdn.laredoute.com/products/250by250/0/6/c/06c48931d22a7c4a5e98ebb79aa8ef1d.jpg" alt="image meuble"><p>Comptoir</p></div>
+          <div class="imgs"><img class = "stock" src="https://cdn.laredoute.com/products/250by250/0/6/c/06c48931d22a7c4a5e98ebb79aa8ef1d.jpg" alt="image meuble"><p>Bar</p></div>
+          <div class="imgs"><img class = "stock" src="https://cdn.laredoute.com/products/250by250/0/6/c/06c48931d22a7c4a5e98ebb79aa8ef1d.jpg" alt="image meuble"><p>Coffre-fort</p></div>
+          <div class="imgs"><img class = "stock" src="https://cdn.laredoute.com/products/250by250/0/6/c/06c48931d22a7c4a5e98ebb79aa8ef1d.jpg" alt="image meuble"><p>Bureau</p></div>
+          <div class="imgs"><img class = "stock" src="https://cdn.laredoute.com/products/250by250/0/6/c/06c48931d22a7c4a5e98ebb79aa8ef1d.jpg" alt="image meuble"><p>Mercerie</p></div>
+          <div class="imgs"><img class = "stock" src="https://cdn.laredoute.com/products/250by250/0/6/c/06c48931d22a7c4a5e98ebb79aa8ef1d.jpg" alt="image meuble"><p>Table</p></div>
+          <div class="imgs"><img class = "stock" src="https://cdn.laredoute.com/products/250by250/0/6/c/06c48931d22a7c4a5e98ebb79aa8ef1d.jpg" alt="image meuble"><p>Canapé</p></div>
+          <div class="imgs"><img class = "stock" src="https://cdn.laredoute.com/products/250by250/0/6/c/06c48931d22a7c4a5e98ebb79aa8ef1d.jpg" alt="image meuble"><p>Lavabo</p></div>
+          <div class="imgs"><img class = "stock" src="https://cdn.laredoute.com/products/250by250/0/6/c/06c48931d22a7c4a5e98ebb79aa8ef1d.jpg" alt="image meuble"><p>Placard</p></div>
+          <div class="imgs"><img class = "stock" src="https://cdn.laredoute.com/products/250by250/0/6/c/06c48931d22a7c4a5e98ebb79aa8ef1d.jpg" alt="image meuble"><p>Pigeonnier</p></div>
+        </div>
+      </div>
+      <div id="contact">
+        <h1>Contact</h1>
+        <p>Le magasin est ouvert le samedi matin de 10h00 à 12h30 ou sur rendez-vous.</p>
+          <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2532.589024112741!2d4.324954415736733!3d50.597591579495955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c232d3694f6c59%3A0xe7333aff0bd54fec!2sRue+Saint-Georges+35%2C+1400+Nivelles!5e0!3m2!1sfr!2sbe!4v1510238505761" width="600" height="450" allowfullscreen></iframe>
+            <p>© 2017 Didier Motte +32 495 20 59 34 - <a href "" alt="Email">dmotte@skynet.be</a> - <a href="/contact.php" alt="Plan d'accès">Plan d'accès</a></p>
+            <p>Décorateur, Antiquaire, agencement d'intérieurs, Belgique</p>
+            <p>Stay update via: <a href="https://www.facebook.com/antiquites.decoration" alt="Facebook" target=_blank>Facebook</a></p>
+          </div>
+          <div class="social">
+          </div>
+      </div>
+    </div>
+  </body>
+</html>
